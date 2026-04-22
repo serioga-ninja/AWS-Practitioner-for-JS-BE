00bfccee11267c8eaf67b94f22abc83abd47a097
serioga-ninjs <sergeidatsiy@gmail.com>
2026-04-22 23:09:28 +0300

    Remove hello lambda stack

--- bin/aws-practitioner-for-js-be.ts
+++ bin/aws-practitioner-for-js-be.ts
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
-import { HelloLambdaStack } from '../lib/hello-lambda/hello-lambda-stack';
 import { ProductServiceStack } from '../lib/product-service/product-service-stack';
 
 const app = new cdk.App();
-
-new HelloLambdaStack(app, 'HelloLambdaStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
 
 new ProductServiceStack(app, 'ProductServiceStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
